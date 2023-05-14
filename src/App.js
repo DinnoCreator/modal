d6759665dfdd8158aeffe5a0eb8ad76712541efd
SmartSHEE <Edidiong Obodom>
2023-05-14 17:46:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [modalIsOpen, setModal] = useState(false);
+
+  const modalHandler = () => {
+    return modalIsOpen ? setModal(false) : setModal(true);
+  };
+
+  const modal = () => {
+    return modalIsOpen ? (
+      <div className="fullWidth fullScreen modal">
+        <div className="centerFlex fullScreen">
+          <div className="bottomShadow flexchild padder">
+            <div className="modal-head">
+                <span className="close right" onClick={modalHandler}>close</span>
+            </div>
+            <div className="modal-body">
+              <h3>I am a modal!</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : (
+      ""
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative">
+      <div className="margAuto">
+        <h1>CodingDrips React Tutorial</h1>
+        <h3>Modal</h3>
+        <button onClick={modalHandler} className="btnct btnct-bluey">
+          Click me!
+        </button>
+      </div>
+      {modal()}
     </div>
   );
 }
